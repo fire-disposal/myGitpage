1054138275d4b07019960fbfe3d77ea5a659f7ef
fire_disposal <3295829485@qq.com>
2024-09-28 20:14:19 +0800

    renamed:    doc/bundle.js -> dist/bundle.js 	renamed:    doc/index.html -> dist/index.html 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/chartLoader.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'doc'),
+    path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
   module: {
