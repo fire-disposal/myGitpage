72a46c2d2d8b9edb85803612d6511232ac368e31
fire_disposal <3295829485@qq.com>
2024-09-29 01:29:38 +0800

    modified:   src/chartList/chart2_1.js 	modified:   src/chartLoader.js 	modified:   src/index.html

--- src/chartList/chart2_1.js
+++ src/chartList/chart2_1.js
@@ -1,15 +1,71 @@
+
 export var option = {
+  2018: {
     title: {
-        text: '示例图表'
+      text: '2018年不同领域文献发文量',
     },
     tooltip: {},
     xAxis: {
-        data: ["A", "B", "C", "D", "E"]
+      type: 'category',
+      data: ['计算机科学', '物理学', '化学', '生物学'],
     },
-    yAxis: {},
+    yAxis: {
+      type: 'value',
+    },
     series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10]
-    }]
+      type: 'bar',
+      data: [120, 150, 90, 110],
+    }],
+  },
+  2019: {
+    title: {
+      text: '2019年不同领域文献发文量',
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: ['计算机科学', '物理学', '化学', '生物学'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [{
+      type: 'bar',
+      data: [130, 160, 100, 115],
+    }],
+  },
+  2020: {
+    title: {
+      text: '2020年不同领域文献发文量',
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: ['计算机科学', '物理学', '化学', '生物学'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [{
+      type: 'bar',
+      data: [140, 170, 110, 120],
+    }],
+  },
+  2021: {
+    title: {
+      text: '2021年不同领域文献发文量',
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: ['计算机科学', '物理学', '化学', '生物学'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [{
+      type: 'bar',
+      data: [150, 180, 120, 125],
+    }],
+  },
 };
--- src/chartLoader.js
+++ src/chartLoader.js
@@ -16,11 +16,11 @@ const optionsList = importAll(require.context("./chartList", false, /\.js$/)); /
 
 document.addEventListener("DOMContentLoaded", function () {
 	// 初始化并挂载图表
-	var Chartinstance1 = echarts.init(document.getElementById("main1"));
-	Chartinstance1.setOption(optionsList.chart1_1); // 修复了这里的变量名
+	var ChartInstance1 = echarts.init(document.getElementById("main1"));
+	ChartInstance1.setOption(optionsList.chart1_1); // 修复了这里的变量名
 
-	var Chartinstance2 = echarts.init(document.getElementById("main2"));
-	Chartinstance2.setOption(optionsList.chart2_1); // 修复了这里的变量名
+	var ChartInstance2 = echarts.init(document.getElementById("main2"));
+	ChartInstance2.setOption(optionsList.chart2_1[2018]); // 修复了这里的变量名
 
 	//初始化文本区域
 	const matchingText = fieldData["chart1_1"] || ""; // 从fieldData中获取描述文本
@@ -28,8 +28,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	// 监听下拉选框变化，更新图表
 	document.getElementById("select1").addEventListener("change", function () {
-		Chartinstance1.clear(); // 清空 chart1
-		Chartinstance1.setOption(optionsList[this.value]); // 修复了这里的访问方式
+		ChartInstance1.clear(); // 清空 chart1
+		ChartInstance1.setOption(optionsList[this.value]); // 修复了这里的访问方式
 
 		// 根据选择项写入匹配的文本到textarea
 		const matchingText = fieldData[this.value] || ""; // 从fieldData中获取描述文本
@@ -38,14 +38,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-	// 监听下拉选框变化，动态更改图表类型
+	// 监听下拉选框变化，更新图表
 	document.getElementById("select2").addEventListener("change", function () {
-		var selectedValue = this.value; // 获取下拉选框的选中值
-
-
-		var currentOption = Chartinstance2.getOption(); // 获取当前图表选项
-		
-		currentOption.series[0].type = selectedValue; // 设置图表类型
-		Chartinstance2.setOption(currentOption); // 应用新的配置选项
-	});
+		ChartInstance2.setOption(optionsList.chart2_1[this.value]); // 更新图表选项
+	  });
 });
